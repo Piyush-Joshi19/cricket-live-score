7a02c5e30f8e9262746dd312d9f9214cd1813cfd
Piyush-Joshi19 <piyushjoshi1901@gmail.com>
2020-12-29 15:07:19 +0530

    Updates

---
+++ precache-manifest.00b0542e3696647208f144a18ec52d49.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab66ffd00d8c9d633ed1d30dda96cd60",
+    "url": "/cricket-live-score/index.html"
+  },
+  {
+    "revision": "e332bdf50c1509844d50",
+    "url": "/cricket-live-score/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "11fcd25b521527475e34",
+    "url": "/cricket-live-score/static/css/main.c1f46776.chunk.css"
+  },
+  {
+    "revision": "e332bdf50c1509844d50",
+    "url": "/cricket-live-score/static/js/2.5c4f660c.chunk.js"
+  },
+  {
+    "revision": "7c31314e008d1a59ddfae5300ece5508",
+    "url": "/cricket-live-score/static/js/2.5c4f660c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11fcd25b521527475e34",
+    "url": "/cricket-live-score/static/js/main.8ba88890.chunk.js"
+  },
+  {
+    "revision": "74075a1c6ef5cc13429b",
+    "url": "/cricket-live-score/static/js/runtime-main.bb78b6f4.js"
+  }
+]);
